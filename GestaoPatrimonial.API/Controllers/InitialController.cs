2b89e9d3d2ce213fcd36ca135d7bd66ecd1df03f
Ricardo izidorio Ferreira <42355541+rizidorio@users.noreply.github.com>
2021-10-20 19:38:54 -0300

    register user initial api

--- GestaoPatrimonial.API/Controllers/InitialController.cs
+++ GestaoPatrimonial.API/Controllers/InitialController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GestaoPatrimonial.Domain.Account;
+using GestaoPatrimonial.Domain.AuthModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GestaoPatrimonial.API.Controllers
 {
@@ -7,10 +9,41 @@ namespace GestaoPatrimonial.API.Controllers
     [ApiController]
     public class InitialController : ControllerBase
     {
+        private readonly IAuthenticate _authenticate;
+
+        public InitialController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate;
+        }
+
         [HttpGet]
         public ActionResult Get()
         {
             return Ok("API OK");
+        }
+
+        [HttpGet]
+        [Route("initalUser")]
+        public ActionResult Create()
+        {
+            try
+            {
+                RegisterUserModel model = new RegisterUserModel
+                {
+                    Email = "user@teste.com",
+                    Password = "teste123",
+                    ConfirmPassword = "teste123",
+                };
+
+                _authenticate.RegisterUser(model);
+
+                return Ok("Usuario criado");
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
